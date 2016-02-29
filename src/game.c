7e114bfc19b89e37c09feabc878dd7b78fdf8633
TheThoron <m.vilao@laposte.net>
2016-02-29 15:17:13 +0100

    SDL / main.c/game.c/h

---
+++ src/game.c
@@ -0,0 +1,126 @@
+#include "game.h"
+
+void play(SDL_Surface* sdlScreen)
+{
+    SDL_Event event;
+	int nInGame;
+	int red;
+	int blue;
+
+	nInGame = 1;
+	red = 0;
+	blue = 0;
+    int Time =0;
+	while(nInGame)
+    {
+    	SDL_PollEvent(&event);
+    	switch(event.type)
+        {
+            case SDL_QUIT:
+                nInGame = 0;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym)
+                {
+                	case SDLK_ESCAPE: // Leave Game
+                        nInGame = 0;
+                        break;
+                    case SDLK_q:
+                    case SDLK_RIGHT:
+                        if (Time > 100000)
+                        {
+                            red = (red + 1)%10;
+                        }
+                        Time++;
+                        break;
+                    case SDLK_d:
+                    case SDLK_LEFT:
+                        if (Time > 100000)
+                        {
+                            red = (red - 1)%10;
+                        }
+                        Time++;
+                        break;
+                }
+                break;
+        }
+
+
+        if (Time > 100000)
+        {
+            Time =0;
+            SDL_FillRect(sdlScreen, NULL, SDL_MapRGB(sdlScreen->format, 255, 255, 255)); 
+        	
+            int x, y;
+            for (x = red; x != 9+red; ++x)
+            {
+                for (y = blue; y != 9+blue; ++y)
+                {
+                    paint(sdlScreen, x, y, (x+y)%3);
+                }    
+            }
+    	    SDL_Flip(sdlScreen);
+            printf("top, \n");
+        }
+        Time++;
+
+    }
+
+}
+
+void paint(SDL_Surface* sdlScreen, int x, int y, int nId)
+{
+    Uint32 color;
+
+    switch(nId)
+    {
+        case 0:
+            color = RED;
+            break;
+        case 1:
+            color = GREEN;
+            break;
+        case 2:
+            color = BLUE;
+            break;
+        default:
+            exit(1);
+    }
+
+    int i, j;
+    for (i = 0; i != SIZE_PIXEL; ++i)
+    {
+        for (j = 0; j != SIZE_PIXEL; ++j)
+        {
+            setPixel(sdlScreen, x*SIZE_PIXEL+i, y*SIZE_PIXEL+j, color);
+        }    
+    }
+}
+
+void setPixel(SDL_Surface *surface, int x, int y, Uint32 color)
+{
+    int bpp = surface->format->BytesPerPixel;
+    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+     
+    if(bpp == 1)
+        *p = color;
+    else if(bpp == 2)
+        *(Uint16*)p = color;
+    else if(bpp == 4)
+        *(Uint32*)p = color;
+    else
+    {
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+        {
+            p[0] = (color >> 16) & 0xff;
+            p[1] = (color >> 8) & 0xff;
+            p[2] = color & 0xff;
+        }
+        else
+        {
+            p[0] = color & 0xff;
+            p[1] = (color >> 8) & 0xff;
+            p[2] = (color >> 16) & 0xff;
+        }
+    }
+}
---
+++ src/main.c
@@ -0,0 +1,66 @@
+#include "../usr2/include/SDL/SDL.h"
+
+#include "game.h"
+
+int main(int argc, char *argv[])
+{
+    SDL_Surface *sdlScreen;
+    SDL_Surface *sdlMenu;
+    SDL_Rect sdlPosMenu;
+    SDL_Event event;
+    int nMenu;
+
+    if ( SDL_Init(SDL_INIT_VIDEO) < 0 )
+    {
+        fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    sdlScreen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    if (sdlScreen == NULL) // Si l'ouverture a échoué, on le note et on arrête
+    {
+        fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_WM_SetCaption("Snake.. SNAKE !", NULL);
+    SDL_ShowCursor(SDL_DISABLE);
+
+    sdlMenu = SDL_LoadBMP("/images/lac_en_montagne.bmp");
+    sdlPosMenu.x = 0;
+    sdlPosMenu.y = 0;
+
+    nMenu = 1;
+    while(nMenu)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                nMenu = 0;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE: // Leave Game
+                        nMenu = 0;
+                        break;
+                    case SDLK_p: // Start Game
+                        play(sdlScreen);
+                        break;
+                }
+                break;
+        }
+
+
+        SDL_FillRect(sdlScreen, NULL, SDL_MapRGB(sdlScreen->format, 20, 20, 20));
+        SDL_BlitSurface(sdlMenu, NULL, sdlScreen, &sdlPosMenu);
+        SDL_Flip(sdlScreen);
+    }
+
+
+    SDL_FreeSurface(sdlMenu);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
