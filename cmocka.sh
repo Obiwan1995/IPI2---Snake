656c4e5678f6b15598dd11f479d77eeebd168bbb
brice <brice.miclo@gmail.com>
2016-03-21 14:56:22 +0100

    Modification du fichier cmocka.sh --> permet d'installer la librairie de tests unitaires

--- cmocka.sh
+++ cmocka.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt-get install cmake -y --force-yes
 
-cd tests/cmocka-1.0.1
+cd lib/cmocka-1.0.1
+rm -Rf build
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
