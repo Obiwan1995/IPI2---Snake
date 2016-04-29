cfe4dbeec39649692d8efceaf09aad2673b806e2
TheThoron <m.vilao@laposte.net>
2016-04-29 19:12:17 +0200

    changement de menu => souligné

--- src/option.c
+++ src/option.c
@@ -26,36 +26,132 @@ void option(SDL_Surface* sdlScreen, int* pNbSnakes, int* pBoard, int* pSpeedInit
 {
     SDL_Surface *sdlOption;
     SDL_Rect sdlPosOption;
-    int nSpeed;
+    SDL_Rect posChoix;
+    SDL_Rect posSpeed;
+    SDL_Rect posSnakes;
+    SDL_Rect posMap;
+    SDL_Rect posWall;
     int nInOption = 1;
     int nChoice = 0;
     int nKeyUp = 1;
 
     sdlPosOption.x = 0;
     sdlPosOption.y = 0;
-    switch(*pSpeedInit)
-    {
-        case SPEED_EASY:
-            nSpeed = 1;
-            break;
-        case SPEED_MEDIUM:
-            nSpeed = 2;
-            break;
-        case SPEED_HARD:
-            nSpeed = 3;
-            break;
-        default:
-            break;
-    }
+    posChoix.h = 2;
+    posSpeed.y = 89;
+    posSpeed.h = 2;
+    posSnakes.y = 185;
+    posSnakes.h = 2;
+    posMap.y = 278;
+    posMap.h = 2;
+    posWall.y = 384;
+    posWall.h = 2;
 
+    sdlOption = SDL_LoadBMP("../images/Option.bmp");       
     SDL_Event event;
     while(nInOption)
     {
-        char strImage[100] = "";
-        sprintf(strImage, "../images/Menu%d%d%d%d.bmp", nSpeed, *pNbSnakes, *pBoard, *pWalls);
-        printf("../images/lac%d speed : %d / %d menu : %d\n", *pBoard, nSpeed, *pSpeedInit, nChoice);
-        sdlOption = SDL_LoadBMP(strImage);
         SDL_BlitSurface(sdlOption, NULL, sdlScreen, &sdlPosOption);
+        switch(nChoice)
+        {
+            case 0:
+                posChoix.x = 332;
+                posChoix.y = 37;
+                posChoix.w = 105;
+                break;
+            case 1:
+                posChoix.x = 323;
+                posChoix.y = 137;
+                posChoix.w = 128;
+                break;
+            case 2:
+                posChoix.x = 349;
+                posChoix.y = 228;
+                posChoix.w = 74;
+                break;
+            case 3:
+                posChoix.x = 332;
+                posChoix.y = 328;
+                posChoix.w = 111;
+                break;
+            default:
+                break;
+        }
+        switch(*pSpeedInit)
+        {
+            case SPEED_EASY:
+                posSpeed.x = 197;
+                posSpeed.w = 75;
+                break;
+            case SPEED_MEDIUM:
+                posSpeed.x = 326;
+                posSpeed.w = 114;
+                break;
+            case SPEED_HARD:
+                posSpeed.x = 495;
+                posSpeed.w = 68;
+                break;
+            default:
+                break;
+        }
+        switch(*pNbSnakes)
+        {
+            case 1:
+                posSnakes.x = 298;
+                posSnakes.w = 15;
+                break;
+            case 2:
+                posSnakes.x = 353;
+                posSnakes.w = 15;
+                break;
+            case 3:
+                posSnakes.x = 408;
+                posSnakes.w = 15;
+                break;
+            case 4:
+                posSnakes.x = 465;
+                posSnakes.w = 15;
+                break;
+            default:
+                break;
+        }
+        switch(*pBoard)
+        {
+            case BOARD_BASE:
+                posMap.x = 175;
+                posMap.w = 123;
+                break;
+            case BOARD_1V1:
+                posMap.x = 360;
+                posMap.w = 44;
+                break;
+            case BIG_BOARD:
+                posMap.x = 485;
+                posMap.w = 49;
+                break;
+            default:
+                break;
+        }
+        switch(*pWalls)
+        {
+            case BOARD_WITH_WALLS:
+                posWall.x = 289;
+                posWall.w = 49;
+                break;
+            case BOARD_WITHOUT_WALLS:
+                posWall.x = 435;
+                posWall.w = 42;
+                break;
+            default:
+                break;
+        }
+
+        SDL_FillRect(sdlScreen, &posChoix, SDL_MapRGB(sdlScreen->format, 206, 206, 0));
+        SDL_FillRect(sdlScreen, &posSpeed, SDL_MapRGB(sdlScreen->format, 206, 206, 206));
+        SDL_FillRect(sdlScreen, &posSnakes, SDL_MapRGB(sdlScreen->format, 206, 206, 206));
+        SDL_FillRect(sdlScreen, &posMap, SDL_MapRGB(sdlScreen->format, 206, 206, 206));
+        SDL_FillRect(sdlScreen, &posWall, SDL_MapRGB(sdlScreen->format, 206, 206, 206));
+
         SDL_Flip(sdlScreen);
 
         SDL_WaitEvent(&event);
@@ -85,15 +181,12 @@ void option(SDL_Surface* sdlScreen, int* pNbSnakes, int* pBoard, int* pSpeedInit
                                     {
                                         case SPEED_EASY:
                                             *pSpeedInit = SPEED_MEDIUM;
-                                            nSpeed = 2;
                                             break;
                                         case SPEED_MEDIUM:
                                             *pSpeedInit = SPEED_HARD;
-                                            nSpeed = 3;
                                             break;
                                         case SPEED_HARD:
                                             *pSpeedInit = SPEED_EASY;
-                                            nSpeed = 1;
                                             break;
                                         default:
                                             break;
@@ -163,15 +256,12 @@ void option(SDL_Surface* sdlScreen, int* pNbSnakes, int* pBoard, int* pSpeedInit
                                     {
                                         case SPEED_EASY:
                                             *pSpeedInit = SPEED_HARD;
-                                            nSpeed = 3;
                                             break;
                                         case SPEED_MEDIUM:
                                             *pSpeedInit = SPEED_EASY;
-                                            nSpeed = 1;
                                             break;
                                         case SPEED_HARD:
                                             *pSpeedInit = SPEED_MEDIUM;
-                                            nSpeed = 2;
                                             break;
                                         default:
                                             break;
@@ -266,7 +356,7 @@ void aide(SDL_Surface* sdlScreen)
 
     SDL_Surface *sdlAide;
     SDL_Rect sdlPosAide;
-    sdlAide = SDL_LoadBMP("../images/MenuV2.bmp");
+    sdlAide = SDL_LoadBMP("../images/Menu.bmp");
     sdlPosAide.x = 0;
     sdlPosAide.y = 0;
     SDL_BlitSurface(sdlAide, NULL, sdlScreen, &sdlPosAide);
