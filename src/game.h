f3fa36645270c97de61863a3ad156d6d7005aee3
TheThoron <m.vilao@laposte.net>
2016-04-29 19:25:39 +0200

    ajout timer

--- src/game.h
+++ src/game.h
@@ -18,16 +18,19 @@
 
 /** Couleur des murs */
 #define WALL 0x005C5C5C
- /** Couleur rouge */
+/** Couleur rouge */
 #define RED 0x00B50000
- /** Couleur verte */
+/** Couleur verte */
 #define GREEN 0x004FEB3B
- /** Couleur bleue */
+/** Couleur bleue */
 #define BLUE 0x0000046E
- /** Couleur jaune */
+/** Couleur jaune */
 #define YELLOW 0x00D9D20B
- /** Couleur des tunnels */
+/** Couleur des tunnels */
 #define TUNNEL 0x00BD8C22
+ 
+/** Seconde avant le début du jeu */
+#define STARTING_TIMER 3
 
 void play(SDL_Surface* sdlScreen, Board board, int nbSnakes, int nSpeedInit);
 void paint(SDL_Surface* sdlScreen, int x, int y, int nId);
