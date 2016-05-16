d593e9c1af4d4ec9d3d94bda0007a6d68535968e
Marie <marie.isabey@orange.fr>
2016-05-16 19:35:48 +0200

    correction du doxyfileé

--- doc/html/annotated.js
+++ doc/html/annotated.js
@@ -1,6 +1,8 @@
 var annotated =
 [
     [ "Board", "struct_board.html", "struct_board" ],
+    [ "Bonus", "struct_bonus.html", "struct_bonus" ],
     [ "Point", "struct_point.html", "struct_point" ],
-    [ "Serpent", "struct_serpent.html", "struct_serpent" ]
+    [ "Serpent", "struct_serpent.html", "struct_serpent" ],
+    [ "Tunnel", "struct_tunnel.html", "struct_tunnel" ]
 ];
--- doc/html/ia_8h.js
+++ doc/html/ia_8h.js
@@ -1,6 +1,6 @@
 var ia_8h =
 [
-    [ "minimum", "ia_8h.html#a26cf52bb4d4af914ff7c3de88375ab13", null ],
+    [ "minimum", "ia_8h.html#a73caefb75490a91b11fa01bb731585fc", null ],
     [ "move_def_ia", "ia_8h.html#afdfe031849d2d1508a0a5c85e256a131", null ],
     [ "move_random_ia", "ia_8h.html#a56edf63e554b1c8be4d61a6cad1f9af5", null ]
 ];
